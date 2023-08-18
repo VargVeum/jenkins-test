8b1d8d4fc87b31ed22bc2a6a6a03b8425fb9661e
Igor Martynenko <igor.martynenko@zuumapp.com>
2023-08-18 16:13:06 +0300

    Added demo tests

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    e2e: {
+      setupNodeEvents(on, config) {
+        allureWriter(on, config);
+        return config;
+    },
+  },
+  allure: true,
+  allureResultsPath: "allure-results",
+});
