fac3a0add290615777c471218616070cf1b662f5
Igor Martynenko <igor.martynenko@zuumapp.com>
2023-09-10 17:58:25 +0300

    Added package.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,8 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: 'QA Report ' + Date.now(),
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-    video: false
-  },
+module.exports = defineConfig({  
   e2e: {
       setupNodeEvents(on, config) {
-        require('cypress-mochawesome-reporter/plugin')(on);
       },  
   },
 });
